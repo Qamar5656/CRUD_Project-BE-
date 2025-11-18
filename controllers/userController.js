73db221d84081a871919ad8748db5ac55da1ff19
Qamar5656 <qamar4157@gmail.com>
2025-11-18 18:47:51 +0500

    adding get request in backend

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,20 +1,61 @@
-// controllers/userController.js
-// export const registerUser = (req, res) => {
-//   console.log("Received user data:", req.body);
-//   res.json({
-//     success: true,
-//     message: "User data received successfully",
-//   });
-// };
-
-// Create user in database
 import User from "../models/userModel.js";
 
+// Create user in database
 export const createUser = async (req, res) => {
   try {
-    const newUser = await User.create(req.body); // Saves to DB
-    res.status(201).json({ success: true, user: newUser });
+    const { firstName, lastName, email, password } = req.body;
+
+    // Check duplicate email
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "Email is already registered",
+      });
+    }
+
+    // Check required fields
+    if (!firstName || !lastName || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+
+    // create user if all things are correct
+    const newUser = await User.create({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password,
+    });
+
+    res.status(201).json({
+      success: true,
+      user: newUser,
+    });
   } catch (error) {
-    res.status(500).json({ success: false, message: error.message });
+    console.error("Backend Error:", error);
+    res.status(500).json({
+      success: false,
+      message: "Server error. Please try again later.",
+    });
+  }
+};
+
+// Get all users from the database
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({
+      success: true,
+      users,
+    });
+  } catch (error) {
+    console.error("Backend Error:", error);
+    res.status(500).json({
+      success: false,
+      message: "Server error. Please try again later.",
+    });
   }
 };
