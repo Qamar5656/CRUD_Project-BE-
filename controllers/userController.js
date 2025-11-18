fec9a7c5e8d1c90474200c48c4e9549d537298d5
Qamar5656 <qamar4157@gmail.com>
2025-11-18 17:06:09 +0500

    user creation in database

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,20 @@
 // controllers/userController.js
-export const registerUser = (req, res) => {
-  console.log("Received user data:", req.body); // this will log data from your React form
-  res.json({
-    success: true,
-    message: "User data received successfully",
-    data: {
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-      password: req.body.password,
-    },
-  });
+// export const registerUser = (req, res) => {
+//   console.log("Received user data:", req.body);
+//   res.json({
+//     success: true,
+//     message: "User data received successfully",
+//   });
+// };
+
+// Create user in database
+import User from "../models/userModel.js";
+
+export const createUser = async (req, res) => {
+  try {
+    const newUser = await User.create(req.body); // Saves to DB
+    res.status(201).json({ success: true, user: newUser });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
 };
