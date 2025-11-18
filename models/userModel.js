1366868d2f9544dfec5f7b87a73509779fb60cd7
Qamar5656 <qamar4157@gmail.com>
2025-11-18 18:48:11 +0500

    adding route in backend

--- models/userModel.js
+++ models/userModel.js
@@ -5,7 +5,6 @@ const userSchema = new mongoose.Schema(
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    // phone: { type: String },
     password: { type: String, required: true },
   },
   { timestamps: true }
