5f84dea62b107fa81791b639882e4a1ae29925dc
Qamar5656 <qamar4157@gmail.com>
2025-11-21 16:30:42 +0500

    updating user routes file

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,18 @@
 // routes/userRoutes.js
 import express from "express";
-import { registerUser } from "../controllers/userController.js";
+import {
+  createUser,
+  getAllUsers,
+  DeleteUsers,
+  UpdateUsers,
+} from "../controllers/userController.js";
+// import { registerUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.post("/register", registerUser);
+router.post("/register", createUser);
+router.get("/users", getAllUsers);
+router.delete("/users/:id", DeleteUsers);
+router.put("/users/:id", UpdateUsers);
 
 export default router;
