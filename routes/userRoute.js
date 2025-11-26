f7d8fb6dbb7a2891e52abcbbf054fc56ab16de4a
Qamar5656 <qamar4157@gmail.com>
2025-11-26 18:43:08 +0500

    implementing multer functionality

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,15 +6,29 @@ import {
   DeleteUsers,
   UpdateUsers,
   SignInUser,
+  VerifyOtp,
+  ResendOtp,
+  forgetPassword,
+  resetPassword,
 } from "../controllers/userController.js";
+import upload from "../utils/multerConfig.js";
 // import { registerUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.post("/register", createUser);
+//CRUD routes
+router.post("/register", upload.single("profileImage"), createUser);
 router.get("/users", getAllUsers);
 router.delete("/users/:id", DeleteUsers);
 router.put("/users/:id", UpdateUsers);
 router.post("/signin", SignInUser);
+
+//Otp routes
+router.post("/verify-otp", VerifyOtp);
+router.post("/resend-otp", ResendOtp);
+
+//Password Routes
+router.post("/forgot-password", forgetPassword);
+router.post("/reset-password", resetPassword);
 
 export default router;
