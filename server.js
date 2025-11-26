1dcae01cc519ddf9c09b5cd5aa2939b6c9a9e510
Qamar5656 <qamar4157@gmail.com>
2025-11-26 11:48:05 +0500

    adding dotenv globally

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
 import userRoutes from "./routes/userRoute.js";
@@ -12,10 +15,10 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-//Routes
+// Routes
 app.use("/api", userRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
